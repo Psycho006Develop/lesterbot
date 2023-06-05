9a2bd3bdf4259c7bd993a98ce7c5d1e5df122e5a
David Markovic <psycho006business@gmail.com>
2023-06-05 20:51:00 +0200

    Updates on npm
    changed locations of some files and updated ytdl-core

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const fs = require("fs");
 const chalk = require("chalk");
 
 const { Client, Collection, GatewayIntentBits, EmbedBuilder } = require("discord.js");
-const { DEFAULT_PREFIX, BOT_TOKEN, ERROR_LOGS_CHANNEL, YT_COOKIE } = require("./config.json");
-const { loadCommands } = require("./handler/loadCommands");
-const { loadEvents } = require("./handler/loadEvents");
-const { loadSlashCommands } = require("./handler/loadSlashCommands")
-const { loadPlayerEvents } = require("./handler/loadPlayerEvents");
+const { DEFAULT_PREFIX, BOT_TOKEN, ERROR_LOGS_CHANNEL, YT_COOKIE } = require("./src/config.json");
+const { loadCommands } = require("./src/handler/loadCommands");
+const { loadEvents } = require("./src/handler/loadEvents");
+const { loadSlashCommands } = require("./src/handler/loadSlashCommands")
+const { loadPlayerEvents } = require("./src/handler/loadPlayerEvents");
 const { DiscordTogether } = require('discord-together');
 const { Player } = require('discord-player');
 const Enmap = require("enmap");
@@ -15,16 +15,16 @@ const client = new Client({
   allowedMentions: { parse: ["users", "roles"] },
   intents: 47007
 });
-const { checkValid } = require("./functions/validation/checkValid");
-const Embeds = require("./functions/embeds/Embeds");
-const Logger = require("./functions/Logger/Logger");
-const Util = require("./functions/util/Util");
+const { checkValid } = require("./src/functions/validation/checkValid");
+const Embeds = require("./src/functions/embeds/Embeds");
+const Logger = require("./src/functions/Logger/Logger");
+const Util = require("./src/functions/util/Util");
 
 client.discordTogether = new DiscordTogether(client);
 client.commands = new Collection();
 client.slash = new Collection();
 client.aliases = new Collection();
-client.categories = fs.readdirSync("./Commands/");
+client.categories = fs.readdirSync("./src/Commands/");
 client.setMaxListeners(0);
 const Cookie = YT_COOKIE;
 client.logger = Logger;
